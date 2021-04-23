d18b294e0fca53bfb5903e20bd55ace1a7249a63
AJ Adams <aj.adams@simplot.com>
2021-04-23 15:31:16 -0600

    age by team

---
+++ average_age_by_team.py
@@ -0,0 +1,23 @@
+import json
+
+with open('./players.json') as p:
+  data = json.load(p)
+
+agedata = {}
+
+for key in data:
+  age = data.get(key).get("age")
+  team = data.get(key).get("team")
+  if not age or team == "OAK":
+    continue
+  if not team:
+    team = "unsigned"
+  if team not in agedata:
+      agedata[team] = []
+  agedata[team].append(age)
+
+for team, ages in agedata.items():
+    print(F"The average age of {team} is {sum(ages) / len(ages):.3}")
+
+
+
